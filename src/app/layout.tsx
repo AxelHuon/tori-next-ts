5478012604c0f9df1552cbbbd56dcc9b810d3730
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-10-25 11:30:16 +0200

    [feature/install-husky-verif-commit][feature]:install husky verif commit and change eslint config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { ThemeProvider } from '@/hooks/useTheme';
 import { device } from '@/utils/device';
-import { usePathname } from 'next/navigation';
 import React from 'react';
 import styled from 'styled-components';
 import MainContainer from '../../layouts/MainContainer/MainContainer';
@@ -20,7 +19,6 @@ const ContainerContentPage = styled.article`
 `;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
   return (
     <html lang="fr">
       <body>
