fbe3129fc158b2724359221b5e73682a1b4c7ae6
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-11-16 09:55:23 +0100

    test

--- prisma/migrations/20231024210904_dqqsdq/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `mal_id` to the `Anime` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Anime` ADD COLUMN `mal_id` INTEGER NOT NULL;
